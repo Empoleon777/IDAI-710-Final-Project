0a6003614aeabff2bff7e887db79c091f68758f0
William Fox <bcf7023@rit.edu>
2026-04-23 20:11:59 -0400

    Some more preliminary stuff.

---
+++ Models/lstm.py
@@ -0,0 +1,23 @@
+import pandas
+import matplotlib.pyplot as plt
+import numpy
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+
+class LSTM():
+    def __init__(self):
+        look_back = 1
+        model = Sequential(
+            LSTM(16, input_shape=(1, look_back)),
+            LSTM(8, input_shape=(1, look_back)),
+            LSTM(4, input_shape=(1, look_back)),
+            LSTM(2, input_shape=(1, look_back)),
+            Dense(1)
+        )
+
+        model.compile(optimizer='adam', loss='mean_squared_error')
+
+    def forward(self, X):
+        logits = self.model(X)
+        return logits
