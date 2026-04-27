8f25ba300f5642b94df2950581002f8653c4ccf2
William Fox <bcf7023@rit.edu>
2026-04-27 18:22:45 -0400

    This looks good for now.

--- Models/lstm.py
+++ Models/lstm.py
@@ -1,53 +1,56 @@
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, BatchNormalization
+from keras.callbacks import EarlyStopping
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score, classification_report, mean_absolute_error
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
 
 class MyLSTM():
     def __init__(self):
-        model = Sequential(
-            LSTM(128),
+        self.model = Sequential([
+            LSTM(64, return_sequences=True, input_shape=(12, 8), recurrent_dropout=0.1),
             Dropout(0.2),
-            LSTM(128),
+            LSTM(32),
             Dropout(0.2),
+            Dense(16, activation='relu'),
+            BatchNormalization(),
             Dense(1)
-        )
+        ])
 
-        model.compile(
+        self.model.compile(
             optimizer='adam', 
             loss='mean_squared_error'
         )
 
-    def Train(self, X_train, y_train, epochs, batch_size, validation_split):
+    def Train(self, X_train, y_train, dates_train, epochs, batch_size, validation_split):
         self.X_train = X_train
         self.y_train = y_train
-        self.training_data = self.model.fit(X_train, y_train, epochs, batch_size, validation_split)
+        self.dates_train = dates_train
+        self.training_data = self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split)
     
-    def Test(self, X_test, y_test):
+    def Test(self, X_test, y_test, dates_test):
         self.X_test = X_test
         self.y_test = y_test
+        self.dates_test = dates_test
         self.predictions = self.model.predict(X_test)
         self.rmse = np.sqrt(np.mean((y_test - self.predictions)**2))
         self.mae = mean_absolute_error(y_test, self.predictions)
-        # print(f"Accuracy: {accuracy_score(y_test, predictions)}")
-        # print(f"Other Metrics: {classification_report(y_test, predictions)}")
     
     def VisualizeActualVsPredicted(self, title):
-        x = self.X_test.columns.tolist()
-
         plt.figure(figsize=(12, 6))
-        plt.plot(x[0], self.y_test, label='Actual Demand')
-        plt.plot(x[0], self.predictions, label='Predicted Demand')
+        plt.plot(self.dates_test, self.y_test, label='Actual Demand')
+        plt.plot(self.dates_test, self.predictions, label='Predicted Demand')
         plt.title(title)
         plt.xlabel('Date')
         plt.ylabel('Demand (Passengers)')
         plt.legend()
-        plt.savefig('../Graphs/actualvspred.jpeg')
+        os.makedirs('../Graphs', exist_ok=True)
+        plt.savefig('Graphs/actualvspred.jpeg')
         plt.show()
 
     def VisualizeLoss(self, title):
@@ -58,53 +61,64 @@ class MyLSTM():
         plt.xlabel('Epoch')
         plt.ylabel('Loss (MSE)')
         plt.legend()
-        plt.savefig('../Graphs/loss.jpeg')
+        os.makedirs('../Graphs', exist_ok=True)
+        plt.savefig('Graphs/loss.jpeg')
         plt.show()
 
     def WritePredsToCSV(self):
-        output = self.X_test[['datetime', 'demand']].copy()
-        output.rename(columns={'demand': 'actual_demand'}, inplace=True)
-        output['predicted_demand'] = self.predictions
-        output.to_csv('../Data/actual_vs_predicted.csv', index=False)
+        output = pd.DataFrame({
+            'datetime': self.dates_test,
+            'actual_demand': self.y_test.flatten(),
+            'predicted_demand': self.predictions.flatten()
+        })
+        os.makedirs('../Results', exist_ok=True)
+        output.to_csv('Results/actual_vs_predicted_lstm.csv', index=False)
+    
+    def PrintMetrics(self):
+        print(f"RMSE: {self.rmse}")
+        print(f"MAE: {self.mae}")
+
+def create_sequences(df, scaled, feature_cols, lookback=12):
+    X = []
+    y = []
+    dates = []
+
+    for i in range(lookback, len(df)):
+        X.append(scaled[i-lookback:i])
+        y.append(scaled[i][0])
+        dates.append(df.index[i])
+
+    return np.array(X), np.array(y), np.array(dates)
 
 data = pd.read_csv('Data/data_with_weather.csv')
-data['Date'] = pd.to_datetime(data['Date'])
-data.set_index('Date', inplace=True)
+data['datetime'] = pd.to_datetime(data['datetime'])
+data.set_index('datetime', inplace=True)
 demand = data['demand'].astype(float).values.reshape(-1, 1)
 
+features = [
+    'demand',
+    'temp',
+    'feelslike',
+    'precip',
+    'snow',
+    'snowdepth',
+    'is_raining',
+    'is_snowing'
+]
+
 scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(demand)
+scaled_data = scaler.fit_transform(data[features])
 
-range_2020 = 7270
-range_2021 = 16030
-range_2022 = 24790
-range_2023 = 28417
+X, y, target_dates = create_sequences(data, scaled_data, features, lookback=12)
 
-
-X_2020 = scaled_data.loc(scaled_data[0:range_2020, ['datetime', 'hour', 'day_of_week', 'is_weekend', 'event', 'temp', 'feelslike', 'precip', 'snow', 'snowdepth', 'is_snowing', 'is_raining']])
-y_2020 = scaled_data.loc(scaled_data[0:range_2020, ['demand']])
-
-X_2021 = scaled_data.loc(scaled_data[range_2020+1:range_2021, ['datetime', 'hour', 'day_of_week', 'is_weekend', 'event', 'temp', 'feelslike', 'precip', 'snow', 'snowdepth', 'is_snowing', 'is_raining']])
-y_2021 = scaled_data.loc(scaled_data[range_2020+1:range_2021, ['demand']])
-
-X_2022 = scaled_data.loc(scaled_data[range_2021+1:range_2022, ['datetime', 'hour', 'day_of_week', 'is_weekend', 'event', 'temp', 'feelslike', 'precip', 'snow', 'snowdepth', 'is_snowing', 'is_raining']])
-y_2022 = scaled_data.loc(scaled_data[range_2021+1:range_2022, ['demand']])
-
-X_2023 = scaled_data.loc(scaled_data[range_2022+1:range_2023, ['datetime', 'hour', 'day_of_week', 'is_weekend', 'event', 'temp', 'feelslike', 'precip', 'snow', 'snowdepth', 'is_snowing', 'is_raining']])
-y_2023 = scaled_data.loc(scaled_data[range_2022+1:range_2023, ['demand']])
-
-X_2020_train, X_2020_test, y_2020_train, y_2020_test = train_test_split(
-    X_2020, y_2020, test_size=0.2, shuffle=False
+X_train, X_test, y_train, y_test, dates_train, dates_test = train_test_split(
+    X, y, target_dates, test_size=0.2, shuffle=False
 )
 
-X_2021_train, X_2021_test, y_2021_train, y_2021_test = train_test_split(
-    X_2021, y_2021, test_size=0.2, shuffle=False
-)
-
-X_2022_train, X_2022_test, y_2022_train, y_2022_test = train_test_split(
-    X_2022, y_2022, test_size=0.2, shuffle=False
-)
-
-X_2023_train, X_2023_test, y_2023_train, y_2023_test = train_test_split(
-    X_2023, y_2023, test_size=0.2, shuffle=False
-)
+model = MyLSTM()
+model.Train(X_train, y_train, dates_train, 250, 32, 0.1)
+model.Test(X_test, y_test, dates_test)
+model.VisualizeActualVsPredicted("Actual Demand vs. Predicted Demand")
+model.VisualizeLoss("LSTM Training Loss")
+model.WritePredsToCSV()
+model.PrintMetrics()
